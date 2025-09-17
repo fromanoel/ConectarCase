e026fce17288ba442a91243f1601184e55367edc
Fernanda Romanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-09-17 13:56:55 -0300

    feat: add header with logo and theme switcher

---
+++ src/components/LogoNav.tsx
@@ -0,0 +1,38 @@
+import logoLight from "../assets/logoLight.svg"
+import logoDark from "../assets/logoDark.svg"
+import { useState, useEffect } from "react"
+import { MdDarkMode, MdLightMode } from "react-icons/md"
+import "../styles/NavBar.css"
+const NavBar = () => {
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    setIsDark(document.body.dataset.theme === "dark")
+  }, [])
+
+  const toggleTheme = () => {
+    const nextTheme = document.body.dataset.theme === "dark" ? "" : "dark"
+    document.body.dataset.theme = nextTheme
+    setIsDark(nextTheme === "dark")
+  }
+  return (
+    <>
+      <nav className="navbar">
+        <img
+          src={isDark ? logoDark : logoLight}
+          alt="Logo PIB"
+          className="logo"
+        />
+        <button className="theme-btn" onClick={toggleTheme}>
+          {isDark ? (
+            <MdLightMode size={24} className="theme-icon" />
+          ) : (
+            <MdDarkMode size={24} className="theme-icon" />
+          )}
+        </button>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
---
+++ src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import "../styles/home.css"
+import HomeSection from "../components/HomeSection.js"
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-headline">
+          <h1 className="home-title">
+            Transforme dados em conhecimento:
+            <span className="home-subtitle">
+              visualize a evolução do PIB brasileiro de forma simples e
+              interativa.
+            </span>
+          </h1>
+        </div>
+        <HomeSection
+          mainTitle="Gráfico de evolução do PIB"
+          mainText="Gráfico linear que exibe a evolução do PIB brasileiro e PIB per capta em dólares."
+        />
+      </div>
+      <div className="home-container">
+        <HomeSection
+          mainTitle="Tabela do PIB por ano"
+          mainText="Tabela que exibe dados referente ao PIB total e per capta em dólares, e o anos referentes."
+        />
+      </div>
+    </>
+  )
+}
+
+export default Home
