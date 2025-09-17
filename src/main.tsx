0755ba81c323aa8461797d501b50427a27c5902f
Fernanda Romanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-09-17 17:24:29 -0300

    feat: add footer component

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,23 @@ import LogoNav from "./components/LogoNav"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Table from "./pages/Table"
 import Chart from "./pages/Chart"
+import Footer from "./components/Footer"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <LogoNav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/table" element={<Table />} />
-        <Route path="/chart" element={<Chart />} />
-      </Routes>
+      <header>
+        {" "}
+        <LogoNav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/table" element={<Table />} />
+          <Route path="/chart" element={<Chart />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   </StrictMode>,
 )
